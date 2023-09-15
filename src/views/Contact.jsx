fd997c17ce07d668ce405291202c3292fdb8e02c
unknown <mboucherba31@gmail.com>
2023-09-15 14:48:35 +0200

    update Contact view

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,7 +1,10 @@
+import { FormulaireInscription } from "./FormulaireInscription";
+
 export const Contact = () => {
     return (
       <>
-        <div>Contact view</div>
+      <h1 className="text-center font-bold text-2xl mt-6">Formulaire de demande d'informations ou d'inscription</h1>
+        <FormulaireInscription />
       </>
     );
-}
+  }
